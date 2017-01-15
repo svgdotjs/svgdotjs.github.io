b91765a415995eabe974c65ca05d14e537278fd5
wout <wout@impinc.co.uk>
2017-01-15 21:56:42 +0100

    Added twitter card

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,17 +3,26 @@
   <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8">
-    <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+    <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@svg_js" />
+    <meta name="twitter:creator" content="@svg_js" />
+    <meta property="og:url" content="<?= $page->url() ?>" />
+    <meta property="og:title" content="<?= $page->title()->html() ?>" />
+    <meta property="og:description" content="<?= $page->description()->html() ?>" />
+    <meta property="og:image" content="<?= $site->url() ?>/assets/images/logo-svg-js-01d.png" />
+
     <meta name="viewport" content="width=device-width">
     <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/png" />
-    <?php echo css('assets/css/main.css') ?>
+    <?= css('assets/css/main.css') ?>
   </head>
   <body class="docs-svgjs">
   <div class="container">
   <header role="banner">
     <div class="wrap">
       <img src="/assets/images/logo-svg-js-01d-128.png" alt="SVG.js logo" class="svgjs-logo">
-      <h1 class="site-title"><a class="title-link" href="<?php echo url() ?>"><?php echo $site->title()->html() ?></a></h1>
+      <h1 class="site-title"><a class="title-link" href="<?= url() ?>"><?= $site->title()->html() ?></a></h1>
       <?php /*
         This is a PHP based search form that will display results using the search template and controller.
         You can also move it around to other places in the templates and snippets.
