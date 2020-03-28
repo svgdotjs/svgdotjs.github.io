2f9c5d6d35c49c88d3635d978c2d0c5df6088f9e
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2020-03-28 21:10:20 +1000

    update script

--- statify.sh
+++ statify.sh
@@ -6,16 +6,13 @@ NEWEST=$(git branch --list "*source*" -r --no-color --points-at origin/HEAD | se
 for i in $BRANCHES; do
   TEMP=$(echo "${i}" | sed -r 's/^.+\///g')
   git checkout -f $TEMP 2>/dev/null || git checkout -f -b $TEMP
+  git pull
   php statify.php
 done
 
 mv docs docs_temp
-git checkout master 2>/dev/null || git checkout -b master
-rm -r docs
+git checkout -f master
+rm -rf docs
 mv docs_temp docs
 
 echo "<?php header('Location: /docs/${NEWEST}/'); ?>" > index.php
-
-git add .
-git commit -m "Update docs"
-git push master
